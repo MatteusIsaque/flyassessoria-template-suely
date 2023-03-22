d3bc4ff9512ab5a83649790f9b7939a930c23bf3
Matteus Isaque <matteus.isaque28@gmail.com>
2023-03-22 10:44:57 -0300

    alteração

--- components/buttonPage2.tsx
+++ components/buttonPage2.tsx
@@ -10,7 +10,7 @@ interface ParamButton {
 export function ButtonZap2({ titulo }: ParamButton) {
 
   return (
-    <Link href="https://api.whatsapp.com/send?phone=5555119911514&text=Ol%C3%A1,%20estou%C2%A0precisando%20de%20ajuda%20para%20ter%20o%20financiamento%20imobili%C3%A1rio%20aprovado.%20Vi%20seu%20an%C3%BAncio%20para%20Mei%20ou%20Micro%20Empres%C3%A1rio,%20voc%C3%AA%20pode%20me%20tirar%20algumas%20d%C3%BAvidas?">
+    <Link href="https://api.whatsapp.com/send?phone=55119911514&text=Ol%C3%A1,%20estou%C2%A0precisando%20de%20ajuda%20para%20ter%20o%20financiamento%20imobili%C3%A1rio%20aprovado.%20Vi%20seu%20an%C3%BAncio%20para%20Mei%20ou%20Micro%20Empres%C3%A1rio,%20voc%C3%AA%20pode%20me%20tirar%20algumas%20d%C3%BAvidas?">
       <button id="buttonZap" className={styles.button} >{titulo}</button>
     </Link>
   )
