6c6516b9a02cf9531a213ae505c53d75a71ba388
Matteus Isaque <matteus.isaque28@gmail.com>
2023-03-22 11:16:02 -0300

    alteração

--- components/buttonPage2.tsx
+++ components/buttonPage2.tsx
@@ -10,7 +10,7 @@ interface ParamButton {
 export function ButtonZap2({ titulo }: ParamButton) {
 
   return (
-    <Link href="https://api.whatsapp.com/send?phone=55119911514&text=Ol%C3%A1,%20estou%C2%A0precisando%20de%20ajuda%20para%20ter%20o%20financiamento%20imobili%C3%A1rio%20aprovado.%20Vi%20seu%20an%C3%BAncio%20para%20Mei%20ou%20Micro%20Empres%C3%A1rio,%20voc%C3%AA%20pode%20me%20tirar%20algumas%20d%C3%BAvidas?">
+    <Link href="https://wa.me/5511991151492?text=Ol%C3%A1%2C+estou+precisando+de+ajuda+para+ter+o+financiamento+imobili%C3%A1rio+aprovado.+Vi+seu+an%C3%BAncio+para+Mei+ou+Micro+Empres%C3%A1rio%2C+voc%C3%AA+pode+me+tirar+algumas+d%C3%BAvidas%3F">
       <button id="buttonZap" className={styles.button} >{titulo}</button>
     </Link>
   )
--- components/firstBackground/buttonZapPage2/index.tsx
+++ components/firstBackground/buttonZapPage2/index.tsx
@@ -9,7 +9,7 @@ export default function ButtonZapFlutuante2() {
 
   return (
     <div id="zapFlutuante" className={styles.ButtonFlutuanteZap}>
-      <Link href="https://api.whatsapp.com/send?phone=5555119911514&text=Ol%C3%A1,%20estou%C2%A0precisando%20de%20ajuda%20para%20ter%20o%20financiamento%20imobili%C3%A1rio%20aprovado.%20Vi%20seu%20an%C3%BAncio%20para%20Mei%20ou%20Micro%20Empres%C3%A1rio,%20voc%C3%AA%20pode%20me%20tirar%20algumas%20d%C3%BAvidas?">
+      <Link href="https://wa.me/5511991151492?text=Ol%C3%A1%2C+estou+precisando+de+ajuda+para+ter+o+financiamento+imobili%C3%A1rio+aprovado.+Vi+seu+an%C3%BAncio+para+Mei+ou+Micro+Empres%C3%A1rio%2C+voc%C3%AA+pode+me+tirar+algumas+d%C3%BAvidas%3F">
         <Image id="zapFlutuante" src={WhatsApp} width={60} height={60} />
       </Link>
     </div>
