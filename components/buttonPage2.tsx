0758504c6253490ab58e8245309e5da6dacde696
Matteus Isaque <matteus.isaque28@gmail.com>
2023-10-28 02:03:04 -0300

    alt

--- components/buttonPage2.tsx
+++ components/buttonPage2.tsx
@@ -9,6 +9,8 @@ interface ParamButton {
 
 export function ButtonZap2({ titulo }: ParamButton) {
 
+
+  
   return (
     <Link href="https://wa.me/5511991151492?text=Estou+interessado%28a%29+no+seu+an%C3%BAncio+de+parceria+com+a+Fly+Assessoria+para+aprova%C3%A7%C3%A3o+de+cr%C3%A9dito+imobili%C3%A1rio.+Voc%C3%AA+pode+me+ajudar%3F">
       <button id="buttonZap" className={styles.button} >{titulo}</button>
